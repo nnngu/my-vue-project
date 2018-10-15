f660191491690012eab4439bb6048fa1d3c87991
nnngu <654015592@qq.com>
2018-10-15 11:50:05 +0800

    Vue.js 目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <!--<router-view/>-->
+        <test></test>
+    </div>
 </template>
 
 <script>
+// 导入组件
+import Test from './components/Test'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Test
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="test">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data: function () {
+    return {
+      msg: '欢迎欢迎！123'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
